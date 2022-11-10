6a3b89111b6b10c6aad38851f869ce1af8d720cb
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-10 21:58:36 +0800

    dark icon

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,18 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Link from 'next/link'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 const Navbar = () => {
     const { theme, setTheme } = useTheme()
+    const [thmeIcon, setThemeIcon] = useState('/dark.svg')
     const changeTheme = () => {
         setTheme(theme === 'light' ? 'dark' : 'light')
     }
+    useEffect(() => {
+      if (theme === 'light' ) {
+        setThemeIcon('/dark.svg')
+      } else {
+        setThemeIcon('/light.svg')
+      }
+    },[theme])
   return (
     <header className="w-full pb-[25px] flex justify-between" >
         <h1 className="logo text-black dark:text-white">
             <Link href="/">Yongzhi</Link>
         </h1>
-        <Image className="cursor-pointer" src={theme === 'light' ? '/dark.svg' : '/light.svg'} alt="theme" width="30" height="30" onClick={changeTheme} />
+        <Image className="cursor-pointer" src={thmeIcon} alt="theme" width="30" height="30" onClick={changeTheme} />
     </header>
   )
 }
