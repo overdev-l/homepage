e7741004e63e5782c2a9480b47f1b70996a9dec9
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-13 17:29:35 +0800

    header

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+import { headers } from './config'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return headers
+  }
 }
 
 module.exports = nextConfig
