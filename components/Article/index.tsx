0d07641383b5c896d6341829af07f91333cd97dd
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-14 20:54:17 +0800

    shouye 优化

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -13,12 +13,12 @@ interface Iprops {
 }
 const Article = ({ post }: Iprops) => {
     return (
-        <article className='flex gap-[20px] items-center'>
-                <span className='font-light text-sm text-neutral-400 dark:text-neutral-200'>{post.frontmatter.date}</span>
-                <h3>
+        <article className='flex items-center'>
+                <h3 className='flex  gap-[20px]'>
+                  <span className='font-light text-sm text-neutral-400 dark:text-neutral-200'>{post.frontmatter.date}</span>
                   <Link
                   href={`/blogs/${post.slug.replace(/\.mdx?$/, '')}`}
-                  className='align-bottom text-base font-normal text-neutral-500 dark:text-neutral-300 underline decoration-pink-500 underline-offset-6'>
+                  className='align-bottom text-sm font-light text-neutral-500 dark:text-neutral-300 underline hover:decoration-pink-500 underline-offset-6'>
                           {post.frontmatter.title}
                   </Link>
                 </h3>
