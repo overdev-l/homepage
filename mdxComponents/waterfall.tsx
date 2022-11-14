e5b0ddc53d2955997ddbbc589498b31d0342ce87
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-14 23:30:23 +0800

    瀑布流

--- mdxComponents/waterfall.tsx
+++ mdxComponents/waterfall.tsx
@@ -1,8 +1,35 @@
-export function Waterfall() {
+import Image from 'next/image'
+import { useState } from 'react'
+interface CardData {
+    source: string
+    description: string
+}
+const Card = ({ source, description }: CardData) => {
     return (
-        <section className="w-full flex px-2.5 gap-y-1" style={{height: '600px'}}>
-            <div className="flex flex-col grow "></div>
-            <div className="flex flex-col grow "></div>
+        <div className="w-full my-1 h-fit">
+            <Image src={source} alt="waterfall" className='w-full h-auto' />
+            <small className='h-6 text-sm text-center truncate'>{description}</small>
+        </div>
+    )
+}
+
+export function Waterfall() {
+    const [leftData, setleftData] = useState<Array<CardData>>([])
+    const [rightData, setRightData] = useState<Array<CardData>>([])
+    return (
+        <section className="w-full" style={{height: '600px'}}>
+            <div className="w-full flex px-2.5 gap-y-1 h-fit">
+                <div className="flex flex-col grow">
+                    {
+                        leftData.map((card, i) => <Card {...card} key={i} />)
+                    }
+                </div>
+                <div className="flex flex-col grow">
+                    {
+                        rightData.map((card, i) => <Card {...card} key={i} />)
+                    }
+                </div>
+            </div>
         </section>
     )
 }
