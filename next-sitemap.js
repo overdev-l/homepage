1403c9cef69700d83aa7fd00930bd49e01a2c3b1
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-20 21:16:47 +0800

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  };
