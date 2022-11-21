300fee035290e4f82b8c3845c1ebb07258d3825c
刘永志 <yongzhi1.liu@gmail.com>
2022-11-21 11:15:53 +0800

    百度验证

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ function MyDocument() {
         return (
             <Html lang="zh-CN">
                 <Head>
+                    <meta name="baidu-site-verification" content="code-7U0mpsvAce" />
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <body>
