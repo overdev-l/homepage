043cb2e0fbaa4fe2f6769c5b18f39a00c31c3242
刘永志 <yongzhi1.liu@gmail.com>
2022-11-21 10:10:31 +0800

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
+    siteUrl: `https://overdev.cn`,
     generateRobotsTxt: true, // (optional)
     // ...other options
   };
