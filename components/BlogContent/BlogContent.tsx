2c0cab5545bbaeb2369f1de1e8f9d2eb4b395bee
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-23 15:48:40 +0800

    remove word count

--- components/BlogContent/BlogContent.tsx
+++ components/BlogContent/BlogContent.tsx
@@ -1,14 +1,12 @@
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote'
 import Waterfall from '../../mdxComponents/waterfall'
 import MoreColumnWaterfall from '../../mdxComponents/moreColumnWaterfall'
-import WordCount from '../../mdxComponents/wordCount'
 interface Iprops {
     mdxSource: MDXRemoteProps,
 }
 const components = {
     Waterfall,
-    MoreColumnWaterfall,
-    WordCount
+    MoreColumnWaterfall
 }
 const BlogContent = ({ mdxSource }: Iprops) => <>
     <MDXRemote {...mdxSource} components={components}></MDXRemote>
