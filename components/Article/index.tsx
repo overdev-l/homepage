554dfacb277f32934ace3d54b5e72e80668f3a43
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-07-11 10:31:57 +0800

    feat: post list css

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -27,18 +27,18 @@ const Article = ({ post }: Iprops) => {
     },[theme])
     return (
         <article className='flex items-center w-full'>
-                <h3 className='flex  gap-[20px] w-full'>
-                  <span className='whitespace-nowrap text-left font-light text-sm text-neutral-400 dark:text-neutral-200 inline-block w-[80px]'>{post.frontmatter.date}</span>
-                  <span className='whitespace-nowrap text-left font-light text-sm text-neutral-400 dark:text-neutral-200 flex items-center gap-2 w-[100px] justify-start'>
+                <section className='grid gap-[10px] grid-cols-8 w-full'>
+                  <span className='whitespace-nowrap text-left font-light text-sm text-neutral-400 dark:text-neutral-200 inline-block w-[80px] flex items-center col-span-2'>{post.frontmatter.date}</span>
+                  <span className='whitespace-nowrap text-left font-light text-sm text-neutral-400 dark:text-neutral-200 flex items-center gap-2 w-[100px] justify-start col-span-2'>
                       <Image src={thmeIcon} alt="reading time" width={10} height={10} className="w-[10px] h-[10px]"></Image>
                     <span className='text-start'>{post.readTime}</span>
                     </span>
                   <Link
                   href={`/blogs/${post.slug.replace(/\.mdx?$/, '')}`}
-                  className='align-bottom w-fit text-sm font-light text-neutral-500 dark:text-neutral-300 underline hover:decoration-pink-500 underline-offset-6'>
+                  className='align-bottom w-fit text-sm font-light text-neutral-500 dark:text-neutral-300 underline hover:decoration-pink-500 underline-offset-6 col-span-4'>
                           {post.frontmatter.title}
                   </Link>
-                </h3>
+                </section>
         </article>
     )
 }
